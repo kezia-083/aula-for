ed7662421fe7d309c4da0eeedba966b5939560ce
kezia-083 <kezialays.ketlyn@gmail.com>
2025-09-24 17:50:21 -0300

    Resolução exercício while

---
+++ trabalho.py
@@ -0,0 +1,63 @@
+print("olá mundo")
+'''
+#1
+nu = 1
+while nu <= 10:
+    print(nu)
+    nu += 1
+
+#2
+nu = 10
+while nu >= 1:
+    print(nu)
+    nu -= 1
+
+#3
+nu = 0
+while nu <= 20:
+    print(nu)
+    nu += 2    
+#4
+nu = 1
+while nu <= 19:
+    print(nu)
+    nu += 2
+#5
+numero = int(input("digite um número"))
+nu = 0
+while nu <= numero:
+    print(nu)
+    nu += 1
+
+#6
+nome = input("digite um palavra")
+nu = 1
+while nu <= 5:
+    print(nome)
+    nu += 1
+
+#7
+nu = 1
+while nu <= 10:
+    print(f"2 x {nu} = {nu * 2}")
+    nu += 1
+
+#8
+numero = int(input("digite um numero"))
+nu = 1
+while nu <= 10:
+    print(f"{numero} x {nu} = {nu * numero}")
+    nu += 1
+'''
+#9
+nu = 1
+while nu <= 5:
+    nu += 1
+    nome = input("digite um nome")
+
+
+
+
+
+
+
